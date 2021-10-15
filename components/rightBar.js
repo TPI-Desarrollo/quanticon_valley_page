27b3265d5633b2f8a5dc524ed8a3d4f6af58449b
ssuarezs <suarez.santiago.113@gmail.com>
2021-10-15 13:20:22 -0500

    fixing rightbar

--- components/rightBar.js
+++ components/rightBar.js
@@ -18,13 +18,14 @@ var RightBar = function RightBar() {
 			React.createElement('div', { id: 'calendario' })
 		),
 		React.createElement(
-			'a',
-			{ type: 'button', className: 'pointer', 'data-toggle': 'modal', 'data-target': '#myModalNews' },
+			'div',
+			{ className: 'right-jumbo' },
 			React.createElement(
-				'div',
-				{ className: 'right-jumbo-news' },
-				React.createElement('iframe', { className: 'noti-video', src: 'https://www.youtube.com/embed/HnVqU8tOAGg', title: 'Noticias', frameBorder: '0', allow: 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture', allowFullScreen: true })
-			)
+				'p',
+				{ className: 'subtext-rb' },
+				'Ultimas Noticias'
+			),
+			React.createElement('iframe', { className: 'noti-video', src: 'https://www.youtube.com/embed/KpILuChm-ko', title: 'Noticias', frameBorder: '0', allow: 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture', allowFullScreen: true })
 		),
 		React.createElement(
 			'a',
@@ -41,43 +42,6 @@ var RightBar = function RightBar() {
 					'div',
 					null,
 					'Conoce las pildoras de esta Semana'
-				)
-			)
-		),
-		React.createElement(
-			'div',
-			{ className: 'modal fade', id: 'myModalNews', tabIndex: '-1', role: 'dialog', 'aria-labelledby': 'myModalLabel' },
-			React.createElement(
-				'div',
-				{ className: 'modal-dialog', role: 'document' },
-				React.createElement(
-					'div',
-					{ className: 'modal-content' },
-					React.createElement(
-						'div',
-						{ className: 'modal-header' },
-						React.createElement(
-							'a',
-							{ type: 'button', 'class': 'close pointer', 'data-dismiss': 'modal', 'aria-label': 'Close' },
-							React.createElement('img', { src: './imgs/exit.png' })
-						),
-						React.createElement(
-							'h4',
-							{ className: 'modal-title', id: 'myModalLabel' },
-							'Noticias'
-						)
-					),
-					React.createElement(
-						'div',
-						{ className: 'modal-body' },
-						'En el transcurso del curso aqui podran ver los noticieros lanzados cada semana',
-						React.createElement(
-							'div',
-							{ className: 'video-wrapper' },
-							React.createElement('iframe', { src: 'https://www.youtube.com/embed/HnVqU8tOAGg', title: 'Noticias', frameBorder: '0', allow: 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture', allowFullScreen: true })
-						)
-					),
-					React.createElement('div', { className: 'modal-footer' })
 				)
 			)
 		)
